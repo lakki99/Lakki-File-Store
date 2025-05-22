c9a4faef4f1d83b58df098634f7d0d991900b13e
lakki99 <pulka143341@gmail.com>
2025-05-22 14:53:50 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -42,7 +42,7 @@ async def broadcast_messages(user_id, message):
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(text='**Broadcasting your messages...**')
+    sts = await message.reply_text(text='**Broadcasting your messages 😹**')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
@@ -70,7 +70,7 @@ async def verupikkals(bot, message):
             done += 1
             if not done % 20:
                 try:
-                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+                    await sts.edit(f"Broadcast in progress🫠:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
                 except:
                     pass
         else:
@@ -79,12 +79,12 @@ async def verupikkals(bot, message):
             failed += 1
             if not done % 20:
                 try:
-                    await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+                    await sts.edit(f"Broadcast in progress🫠:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
                 except:
                     pass
     
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Broadcast Completed🫡:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
