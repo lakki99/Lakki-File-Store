1de1ee3a9b29dc2340b05f64065c80826e45a5b1
Lakki reddy <pulka143341@gmail.com>
2025-07-05 21:18:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "d3cd72e44e4160ce1ee0b7947e8f2d89")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/7c6df20032fe4e6a2a729-c5ecb08505386ca643.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8134543713 5665480584 7460363208').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8134543713').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Sakura_1bot") # without @
 PORT = environ.get("PORT", "8080")
 
