ab5b27f353df67f4ada1cbc98389047c46d54aac
Lakki reddy <pulka143341@gmail.com>
2025-07-05 21:06:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,12 +23,12 @@ API_HASH = environ.get("API_HASH", "d3cd72e44e4160ce1ee0b7947e8f2d89")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/7c6df20032fe4e6a2a729-c5ecb08505386ca643.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8134543713 5665480584').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8134543713 5665480584 7460363208').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Sakura_1bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://enobot23456:sHpPRLjXl37caboA@cluster0.xtptuxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
